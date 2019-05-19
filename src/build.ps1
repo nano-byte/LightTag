3224d97ed5d050ef316e8805f23689e0f7c9fd97
Bastian Eicher <bastian@eicher.net>
2019-05-19 20:24:53 +0200

    Updated build script to support for Visual Studio 2019

--- src/build.ps1
+++ src/build.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
 $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
-$msBuild = "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+$msBuild = if (Test-Path "$vsDir\MSBuild\Current") {"$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe"} else {"$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"}
 . $msBuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=$Version
 
 popd
